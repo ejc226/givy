b446bc0a36b61968784c2c51dc49badb7c61404d
Eric <ericjiwonchoi@gmail.com>
2014-02-12 17:48:46 -0800

    class project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Givy::Application.config.secret_key_base = '39ecdc3e716417659b9acd7210fd52cb22aa8a15edbd3a6d6efabe5005c1919314518150a3a4c38d9ab9c54d533f72031429f9fb833dffaedcb1aaf5c451dacc'
